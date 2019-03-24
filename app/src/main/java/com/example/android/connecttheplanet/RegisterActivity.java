db88c171d09140889942d15ca7a10d050d15d979
Rebecca <rebeccagolm32@gmail.com>
2019-03-23 23:37:41 -0400

    register

--- app/src/main/java/com/example/android/connecttheplanet/RegisterActivity.java
+++ app/src/main/java/com/example/android/connecttheplanet/RegisterActivity.java
@@ -117,7 +117,7 @@ public class RegisterActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             //Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(RegisterActivity.this, "Authentication failed."+email+" "+password,
+                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             //updateUI(null);
                         }
