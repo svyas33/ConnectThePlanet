e33f2d32a15cf41f6f91db7b46b1925e8e9e43fc
ShivaniVyas <shivanivyas33@gmail.com>
2019-03-24 00:19:33 -0400

    add img

--- app/src/main/java/com/example/android/connecttheplanet/CreateProjectActivity.java
+++ app/src/main/java/com/example/android/connecttheplanet/CreateProjectActivity.java
@@ -1,10 +1,14 @@
 package com.example.android.connecttheplanet;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -17,8 +21,10 @@ public class CreateProjectActivity extends AppCompatActivity {
 
     String projName, projDetails,orgEmail;
     int numVolunteers, costOfProject;
+    ImageView projImg;
     Button create;
     private FirebaseAuth mAuth;
+    Uri resultUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +39,8 @@ public class CreateProjectActivity extends AppCompatActivity {
 
         numVolunteers = Integer.parseInt(((EditText) findViewById(R.id.create_numVolunteersTextView)).getText().toString());
         costOfProject = Integer.parseInt(((EditText) findViewById(R.id.create_moneyNeededTextView)).getText().toString());
+
+        projImg = findViewById(R.id.create_imageView);
 
         final DatabaseReference currentProjectDb = FirebaseDatabase.getInstance().getReference().child("Project");
 
@@ -52,7 +60,27 @@ public class CreateProjectActivity extends AppCompatActivity {
             }
         });
 
+        ImageView addProjImg = findViewById(R.id.create_addImageButton);
+        addProjImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
 
 
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            final Uri imageUri = data.getData();
+            resultUri = imageUri;
+            projImg.setImageURI(resultUri);
+        }
     }
 }
